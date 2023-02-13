0995a3e0d17c8753a06c3fdfbacb0cc0bdfd4b00
ArshiaKSabet <arshia.kolachaei@hotmail.com>
2023-02-13 22:50:37 +0100

    adding fetch function, added onSubmit button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const dataInput = document.querySelector('#data');
 
+// const imageFormat = document.querySelector('input[name="format"]:checked');
+
 const primaryColorPicker = document.querySelector('#color');
 const bgColorPicker = document.querySelector('#bg-color');
 
@@ -22,3 +24,65 @@ const addColorPickerEventListeners = () => {
 };
 
 addColorPickerEventListeners();
+
+const sizeSlider = document.querySelector('#size');
+const marginSlider = document.querySelector('#margin');
+
+const sizeValue = document.querySelector('#size-value');
+const marginValue = document.querySelector('#margin-value');
+
+const updateSize = e => {
+    const value = e.target.value;
+    sizeValue.innerText = `${value} x ${value}`;
+};
+
+const updateMargin = e => {
+    const value = e.target.value;
+    marginValue.innerText = `${value} px`;
+};
+
+const addSliderEventListeners = () => {
+    sizeSlider.addEventListener('change', updateSize);
+    marginSlider.addEventListener('change', updateMargin);
+};
+
+addSliderEventListeners();
+
+const prepareParameters = params => {
+    return {
+        data: params.data,
+        size: `${params.size}x${params.size}`,
+        color: params.color.replace('#', ''),
+        bgcolor: params.bgColor.replace('#', ''),
+        qzone: params.qZone,
+        format: params.format,
+    };
+};
+
+const submitButton = document.querySelector('#cta');
+
+const getQrCode = parameters => {
+    const baseUrl = 'http://api.qrserver.com/v1/create-qr-code/';
+    const urlParams = new URLSearchParams(parameters).toString();
+
+    fetch(`${baseUrl}?${urlParams}`);
+};
+
+const onSubmit = () => {
+    const data = dataInput.value;
+    const color = primaryColorPicker.value;
+    const bgColor = bgColorPicker.value;
+    const size = sizeSlider.value;
+    const qZone = marginSlider.value;
+    const format = document.querySelector('input[name="format"]:checked.value');
+
+    const parameters = prepareParameters({ data, color, bgColor, size, qZone, format });
+
+    getQrCode(parameters);
+};
+
+const addSubmitEventlistener = () => {
+    submitButton.addEventListener('click', onSubmit);
+};
+
+addSubmitEventlistener();
