33564e81914958bec928334cf020fa245498d65c
ArshiaKSabet <arshia.kolachaei@hotmail.com>
2023-02-13 17:45:02 +0100

    colorpickers added and now working

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("JS loaded");
+const dataInput = document.querySelector('#data');
+
+const primaryColorPicker = document.querySelector('#color');
+const bgColorPicker = document.querySelector('#bg-color');
+
+const primaryColorValue = document.querySelector('#color-value');
+const bgColorValue = document.querySelector('#bg-color-value');
+
+const updateColor = e => {
+    const value = e.target.value;
+    primaryColorValue.innerText = value;
+};
+
+const updateBgColor = e => {
+    const value = e.target.value;
+    bgColorValue.innerText = value;
+};
+
+const addColorPickerEventListeners = () => {
+    primaryColorPicker.addEventListener('change', updateColor);
+    bgColorPicker.addEventListener('change', updateBgColor);
+};
+
+addColorPickerEventListeners();
